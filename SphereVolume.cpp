b608122c53195c67e2497160aeb2032f4175d0f7
İsmail Can Yıldırım <ismailcan5106@gmail.com>
2022-10-10 22:15:12 +0300

    SphereVolume

---
+++ SphereVolume.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int pi,r;
+	pi=3.14;
+	cout<<"*********Kürenin Hacmini Bulma********* \n";
+	cout<<"Kürenin Yarýçapýný Giriniz : ";
+	cin>> r ;
+	cout<<"\n\nKürenin Hacmi:"<<pi*r*r<<endl;
+}
